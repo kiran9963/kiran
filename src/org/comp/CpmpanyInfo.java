32f44739a27cfa630233b7984f6ecec6ae10d27c
kiran9963 <kranthikiran7386@gmail.com>
2019-07-31 09:44:41 +0530

    tast

---
+++ src/org/comp/CpmpanyInfo.java
@@ -0,0 +1,20 @@
+package org.comp;
+
+public class CpmpanyInfo {
+	
+		public void cpmpanyId(int id) {
+			System.out.println("company id is:"+id);
+			
+		}
+		public void cpmpanyname(String name) {
+			System.out.println("name is"+name);
+		}
+		public static void main(String[] args) {
+			CpmpanyInfo c=new CpmpanyInfo();
+			c.cpmpanyId(1234);
+			c.cpmpanyname("kjhdka@sdbhasjd76");
+			
+		}
+	}
+
+
